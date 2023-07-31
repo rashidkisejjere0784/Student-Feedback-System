c1d2e6383518ea63b3ce8b1fffb5d124d65256ac
Priscillasky <priscillasky542@gmail.com>
2023-07-31 15:58:39 +0300

    commit

--- home/views.py
+++ home/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def index(request):
 
     # Page from the theme 
-    return render(request, 'pages/index.html')
+    if request.user.is_authenticated:
+        return render(request, 'pages/index.html')
+    
+    else:
+        return redirect("login")
 
 def facility(request):
     return render(request, 'pages/facility.html')
