648718fb65d78067be307ee869be03479277c695
kisejjere rashid <rashidkisejjere0784@gmail.com>
2023-07-31 14:52:22 +0300

    first commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+
+    # Page from the theme 
+    return render(request, 'pages/index.html')
+
+def facility(request):
+    return render(request, 'pages/facility.html')
